732a6fce7b2629529a883065a0859fd689e05b17
Julien Minguely <jminguely@gmail.com>
2018-08-17 10:32:08 +0200

    Play with some env variable

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
 
+let SERVER_PORT = 8101;
+let SERVER_IP = "0.0.0.0";
+
+console.log(process.env.NODE_ENV);
+console.log('No value for FOO yet:', process.env.FOO);
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+  SERVER_PORT = "127.0.0.1";
+  SERVER_IP = 3000;
+}
+
+console.log('Now the value for FOO is:', process.env.FOO);
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! TEST', process)
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(SERVER_PORT, SERVER_IP, function () {
+  console.log(`Example app listening on: ${SERVER_IP}:${SERVER_PORT}`)
 })
