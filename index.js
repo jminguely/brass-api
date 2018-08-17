b7f531213b352f4f4e11c35824f45e58b20051a4
Julien Minguely <jminguely@gmail.com>
2018-08-17 11:09:25 +0200

    It works

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ console.log(process.env.SERVER_PORT);
 console.log(process.env.SERVER_IP);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('It Works')
 })
 
 app.listen(process.env.SERVER_PORT, process.env.SERVER_IP, function () {
   console.log(`Example app listening on: ${process.env.SERVER_IP}:${process.env.SERVER_PORT}`)
 })
+ca
