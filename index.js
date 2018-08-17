b8a314c3a0b49ccd8fc92b00d5e46ceb3582a319
Julien Minguely <jminguely@gmail.com>
2018-08-17 11:30:05 +0200

    It works really...

--- index.js
+++ index.js
@@ -12,4 +12,3 @@ app.get('/', function (req, res) {
 app.listen(process.env.SERVER_PORT, process.env.SERVER_IP, function () {
   console.log(`Example app listening on: ${process.env.SERVER_IP}:${process.env.SERVER_PORT}`)
 })
-ca
