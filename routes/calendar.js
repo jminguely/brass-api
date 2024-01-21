9363090cd571516e6b0861bbaecb87bd578c4cec
Julien Minguely <jminguely@gmail.com>
2024-01-21 15:10:20 +0100

    Fixes the calendar

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,35 +1,46 @@
 const express = require('express');
 const router = express.Router();
 const base = require('../config/airtable');
+const moment = require("moment");
+moment.locale("fr_FR");
 const formatDate = require('../utils/formatDate');
+const ics = require('ics');
 
-const TABLE_NAME = 'Agenda';
+const MAX_RECORDS = 1000;
+const CONCERTS_TABLE = "Concerts";
 const FILTER_FORMULA = "OR({Statut} = 'Booking', {Statut} = 'Confirmé')";
 
 router.get('/calendar', async (req, res) => {
   try {
-    const records = await base(TABLE_NAME).select({
-      filterByFormula: FILTER_FORMULA,
-      sort: [{ field: "Date de début", direction: "asc" }]
-    }).all();
+    const concerts = await base(CONCERTS_TABLE)
+      .select({
+        maxRecords: MAX_RECORDS,
+        filterByFormula: FILTER_FORMULA,
+        sort: [{ field: "Date check-in", direction: "asc" }],
+      })
+      .all();
 
-    const events = records.map(record => {
-      const startDate = moment(record.get('Date de début'));
-      const endDate = moment(record.get('Date de fin'));
+    const events = concerts.map(concert => {
+      const { fields } = concert;
+      const date_start = moment(fields['Date check-in']);
+      const date_end = moment(fields['Date fin']);
+      const start = date_start.format('YYYY-M-D-H-mm').split("-").map(Number);
+      const end = fields['Date fin'] ? date_end.format('YYYY-M-D-H-mm').split("-").map(Number) : start;
+
       return {
-        title: record.get('Nom'),
-        start: startDate.format(),
-        end: endDate.format(),
-        extendedProps: {
-          date: formatDate(startDate, endDate),
-          location: record.get('Lieu'),
-          status: record.get('Statut'),
-          musicians: record.get('Musiciens'),
-        },
+        title: `BMF - ${fields['Titre'] || fields['Type']}`,
+        location: fields['Ville'],
+        description: `${fields['API']}\n\n${fields['Informations'] || ""}`,
+        url: fields['API'],
+        start,
+        end
       };
     });
 
-    res.json(events);
+    const { error, value } = ics.createEvents(events);
+    req.is('text/calendar')
+    res.send(value);
+
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred while fetching calendar events.');
